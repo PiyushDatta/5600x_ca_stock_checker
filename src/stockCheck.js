1005e85dcdf45678dfbf287d06ab13fc5cddb0d5
piyushdatta <piyushdattaca@gmail.com>
2020-12-05 18:24:02 -0500

    got rid of individual stock checker files, since the only difference is the selectors itself.

--- src/stockCheck.js
+++ src/stockCheck.js
@@ -1,32 +1,48 @@
-const amazonStockCheck = require("./urlStockCheckers/amazonStockCheck");
-const neweggStockCheck = require("./urlStockCheckers/neweggStockCheck");
-const canadaComputersStockCheck = require("./urlStockCheckers/canadaComputersStockCheck");
+const utils = require("./utils");
 
 async function getProductDetails(url) {
   switch (true) {
+    // AMAZON
     case url.includes("amazon"):
+      selectors = {
+        productName: "#productTitle",
+        productPrice: "#price_inside_buybox",
+        productAvailability: "#availability .a-size-medium",
+      };
       return (
         "Site: Amazon\n" +
         "Link: " +
         url +
         "\n" +
-        (await amazonStockCheck.getAmazonProductDetails(url))
+        (await utils.getProductDetails(url, selectors))
       );
+    // NEWEGG
     case url.includes("newegg"):
+      selectors = {
+        productName: ".product-title",
+        productPrice: ".product-price .price-current",
+        productAvailability: ".product-inventory",
+      };
       return (
         "Site: Newegg\n" +
         "Link: " +
         url +
         "\n" +
-        (await neweggStockCheck.getNeweggProductDetails(url))
+        (await utils.getProductDetails(url, selectors))
       );
+    // CANADA COMPUTERS
     case url.includes("canadacomputers"):
+      selectors = {
+        productName: ".h3.mb-0",
+        productPrice: ".h2-big",
+        productAvailability: ".pi-prod-availability span",
+      };
       return (
         "Site: Canada Computers\n" +
         "Link: " +
         url +
         "\n" +
-        (await canadaComputersStockCheck.getCanadaComputersProductDetails(url))
+        (await utils.getProductDetails(url, selectors))
       );
     default:
       return "No scraper found for: " + url;
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,37 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
+async function getProductDetails(url, selectors) {
+  var html = await getHTMLPage(url);
+
+  // product name
+  var defaultValue = "Could not find product name.";
+  const productName = await scrapeProductSite(
+    html,
+    defaultValue,
+    selectors["productName"]
+  );
+
+  // product price
+  defaultValue = "0.00";
+  var productPrice = await scrapeProductSite(
+    html,
+    defaultValue,
+    selectors["productPrice"]
+  );
+  productPrice = Number(productPrice.replace(/[^0-9.-]+/g, ""));
+
+  // product availability
+  defaultValue = "Could not find product availability.";
+  const productAvailability = await scrapeProductSite(
+    html,
+    defaultValue,
+    selectors["productAvailability"]
+  );
+
+  return `Product name: ${productName}\nPrice: ${productPrice}\nAvailability: ${productAvailability}`;
+}
+
 async function getHTMLPage(url) {
   // browser vars
   const browser = await puppeteer.launch();
@@ -16,4 +47,4 @@ async function scrapeProductSite(html, defaultValue, selector) {
   return defaultValue;
 }
 
-module.exports = { getHTMLPage, scrapeProductSite };
+module.exports = { getProductDetails };
